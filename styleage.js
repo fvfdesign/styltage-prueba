1a56da3a5fbc46c324afa232f7b48e835e4ea73d
Fernanda <fvfdesign@gmail.com>
2016-08-04 10:53:37 +0200

    prueba Syle

---
+++ styleage.js
@@ -0,0 +1,57 @@
+// var count = 0;
+
+// $("#image").click(function() {
+//     count++;
+//     $("#counter").html("your count: "+count);
+// }
+
+//......................................
+
+// 	$("#image").on('click',function(){
+
+// 	var image = true;
+// 	var count = 10;
+
+// 	while (image) {
+//     console.log(count);
+//     if (count < 10) {
+//             image = false;
+//     }
+//     count++;
+// }
+
+// 		console.log ("")
+// }
+
+// counter();
+
+//.......................................
+
+
+
+// 	$("#image").on('click',function(){
+
+// 	var string = "";
+
+// 	for (var i= 0; i<10; i++ ){
+// 		string += "1";
+// 		console.log (string);
+// 	}
+
+
+//...............
+
+
+	$('#image').on('click',function(){
+	var image;
+	for(image = 0; image<10; image++) {
+	console.log("1");
+	}
+}
+// }
+
+
+
+
+
+
